6b3d04c5f31174ae17d222dad2dc3611c053daee
jplovesraw@gmail.com <jplovesraw@gmail.com>
2026-03-15 09:03:03 +0800

    chore: removed old hono server
    only keeps the bun elysia server

--- server/prisma/migrations/20260227004322_add_sale_unique_index/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,createdAt]` on the table `Sale` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Sale_userId_createdAt_key" ON "Sale"("userId", "createdAt");
--- server/src/db/dbClient.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL || "" });
-const dbClient = new PrismaClient({ adapter: pool });
-
-export default dbClient;
